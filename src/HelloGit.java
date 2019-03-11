0b68cb323396a3fbeef38aa06dc6ad2da43a565f
admin <admin@itcast.cn>
2019-03-11 21:08:08 +0800

    修改hello

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,6 +3,5 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("abc");
         System.out.println("abc");
-        System.out.println("abc");
     }
 }
